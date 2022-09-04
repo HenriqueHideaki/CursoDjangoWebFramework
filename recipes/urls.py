fa0adcbf561b3f75bca8f23338561d4751688851
Henrique Hideaki <hideakikoga.hk@gmail.com>
2022-09-03 21:54:54 -0300

    segundo commit

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,8 +1,12 @@
 from django.http import HttpResponse
-from recipes.views import home
-from django.urls import path, include
+from django.urls import include, path
 
+from . import views
+
+# recipes:recipe
+app_name = 'recipes'
 
 urlpatterns = [
-    path('', home), #home
+    path('', views.home, name="home"),  # home
+    path('recipes/<int:id>/', views.recipe, name="recipe"),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,10 +1,20 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
+from utils.recipes.factory import make_recipe
+
+from recipes.models import Recipe
 
 
 # Create your views here.
 def home(request):
-    return render(request, 'recipes/pages/home.html', context = {
-        'nome' : 'Henrique',
+    recipes = Recipe.objects.all().order_by('-id')
+    return render(request, 'recipes/pages/home.html', context={
+        'recipes': recipes,
     })
 
+
+def recipe(request, id):
+    return render(request, 'recipes/pages/recipe-view.html', context={
+        'recipe': make_recipe(),
+        'is_detail_page': True,
+    })
